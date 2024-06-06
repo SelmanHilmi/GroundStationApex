83afa43aaf02af1ef4639840bddd91a1abf0db7a
SelmanHilmi <141406670+SelmanHilmi@users.noreply.github.com>
2024-06-06 15:50:10 +0300

    cakisma 1

--- Ground Station of Apex/Ground Station of Apex/Form1.Designer.cs
+++ Ground Station of Apex/Ground Station of Apex/Form1.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(46)))));
-            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.ClientSize = new System.Drawing.Size(1896, 1080);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "APEX AMK";
